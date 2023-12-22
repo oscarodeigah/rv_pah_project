919b5b2eed1cc4dfbdc2b7df54b2c89f084dea35
Oscar Odeigah <oscar.odeigah@gmail.com>
2023-12-23 00:53:42 +0100

    Resolved conflict and accepted changes

--- src/rv_pah_project/model_optimization.py
+++ src/rv_pah_project/model_optimization.py
@@ -134,15 +134,9 @@ def passive_optimization(
         logger.info(f"\nCurrent x: {x}")
 
     x0 = np.array([initial_rv_matparam_a])
-<<<<<<< HEAD:src/rv_pah_project/model_optimization.py
-    bnds = [(0.05, 10)]
-
-    logger.info("\nStarting passive optimization")
-=======
     bnds = [(0.07, 10)]
     
     logger.info(f"\nStarting passive optimization")
->>>>>>> origin/main:scripts/model_optimization.py
     start = datetime.now()
     res = minimize(
         objective, x0=x0, bounds=bnds, options={"disp": 5}, callback=print_control
